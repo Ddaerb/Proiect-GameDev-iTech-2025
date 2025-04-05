c3b9f6b1097e45537611171c70fb2922b0b7d56a
Harry <harypatrascu.1@gmail.com>
2025-04-05 14:40:13 +0300

    Added hats

--- Assets/Scripts/CharacterCreation.cs
+++ Assets/Scripts/CharacterCreation.cs
@@ -6,13 +6,21 @@ using UnityEngine.UI;
 public class CharacterCreation : MonoBehaviour
 {
     [SerializeField] GameObject[] _characters;
+    [SerializeField] GameObject[] _hats;
     [SerializeField] Button _nextCharButton;
     [SerializeField] Button _prevCharButton;
+    [SerializeField] Button _nextHatButton;
+    [SerializeField] Button _prevHatButton;
     [SerializeField] TextMeshProUGUI _characterNameText;
+    [SerializeField] TextMeshProUGUI _hatNameText;
 
+    private Vector3[] _hatSizes;
     private GameObject _selectedCharacter;
+    private GameObject _selectedHat;
     private GameObject _instantiatedCharacter;
+    private GameObject _instantiatedHat;
     private int _selectedCharacterIndex = 0;
+    private int _selectedHatIndex = 0;
 
 
     private void Awake()
@@ -20,6 +28,8 @@ public class CharacterCreation : MonoBehaviour
         // Add button events
         _nextCharButton.onClick.AddListener(NextCharacterSelect);
         _prevCharButton.onClick.AddListener(PrevCharacterSelect);
+        _nextHatButton.onClick.AddListener(NextHatSelect);
+        _prevHatButton.onClick.AddListener(PrevHatSelect);
 
         // Update the character name text to the first character in the array
         _characterNameText.text = _characters[_selectedCharacterIndex].name;
@@ -27,12 +37,61 @@ public class CharacterCreation : MonoBehaviour
         // Set the first character from the characters array as selected
         _selectedCharacter = _characters[_selectedCharacterIndex];
         _instantiatedCharacter = Instantiate(_selectedCharacter, transform);
+
+        // Set the hat sizes
+        SetHatSizes();
+    }
+
+    private void SetHatSizes()
+    {
+        _hatSizes = new Vector3[_hats.Length];
+        // Set the hat sizes for each hat
+        _hatSizes[0] = Vector3.zero;
+        _hatSizes[1] = new Vector3(0.01f, 0.01f, 0.01f);
+        _hatSizes[2] = new Vector3(0.003f, 0.002f, 0.003f);
+    }
+
+    private void PrevHatSelect()
+    {
+        Transform hatTransform = FindChildWithTag();
+        if (hatTransform != null)
+        {
+            Destroy(_instantiatedHat);
+            if(_selectedHatIndex == 0)
+            {
+                _selectedHatIndex = _hats.Length - 1;
+            }
+            else
+            {
+                _selectedHatIndex--;
+            }
+            UpdateHat(hatTransform);
+        }
+        else
+        {
+            Debug.LogWarning("No HatTransform found for: " + _selectedCharacter.name);
+        }
+    }
+
+    private void NextHatSelect()
+    {
+        Transform hatTransform = FindChildWithTag();
+        if (hatTransform != null)
+        {
+            Destroy(_instantiatedHat);
+            _selectedHatIndex = (_selectedHatIndex + 1) % _hats.Length;
+            UpdateHat(hatTransform);
+        }
+        else
+        {
+            Debug.LogWarning("No HatTransform found for: " + _selectedCharacter.name);
+        }
     }
 
     private void PrevCharacterSelect()
     {
         Destroy(_instantiatedCharacter);
-        if(_selectedCharacterIndex == 0)
+        if (_selectedCharacterIndex == 0)
         {
             _selectedCharacterIndex = _characters.Length - 1;
         }
@@ -48,6 +107,8 @@ public class CharacterCreation : MonoBehaviour
         Destroy(_instantiatedCharacter);
         _selectedCharacterIndex = (_selectedCharacterIndex + 1) % _characters.Length;
         UpdateCharacter();
+        Transform hatTransform = FindChildWithTag();
+        UpdateHat(hatTransform);
     }
 
     private void UpdateCharacter()
@@ -55,5 +116,26 @@ public class CharacterCreation : MonoBehaviour
         _characterNameText.text = _characters[_selectedCharacterIndex].name;
         _selectedCharacter = _characters[_selectedCharacterIndex];
         _instantiatedCharacter = Instantiate(_selectedCharacter, transform);
+    }
+
+    private void UpdateHat(Transform hatTransform)
+    {
+        _hatNameText.text = _hats[_selectedHatIndex].name;
+        _selectedHat = _hats[_selectedHatIndex];
+        _selectedHat.transform.localScale = _hatSizes[_selectedHatIndex];
+        _instantiatedHat = Instantiate(_selectedHat, hatTransform);
+    }
+
+    private Transform FindChildWithTag()
+    {
+        Transform[] children = _instantiatedCharacter.GetComponentsInChildren<Transform>();
+        foreach (Transform child in children)
+        {
+            if (child.CompareTag("HatTransform"))
+            {
+                return child;
+            }
+        }
+        return null;
     }
 }
