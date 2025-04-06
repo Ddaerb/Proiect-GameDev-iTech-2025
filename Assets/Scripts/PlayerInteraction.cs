c644b8599ee15183ebfa93652742ac43b320a7d3
Harry <harypatrascu.1@gmail.com>
2025-04-06 03:34:40 +0300

    More work on npc interaction

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -3,23 +3,46 @@ using UnityEngine;
 public class PlayerInteraction : MonoBehaviour
 {
     // The radius within which the player can interact with objects
-    private float _interactionRadius = 12f;
+    private float _interactionRadius = 2f;
+    private PlayerMovement _playerMovement;
+
+    private void Awake()
+    {
+        // Get the PlayerMovement component attached to the player
+        _playerMovement = GetComponent<PlayerMovement>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.E))
+        CheckNearbyNPC();
+    }
+
+    private void CheckNearbyNPC()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
         {
             Collider[] colliders = Physics.OverlapSphere(transform.position, _interactionRadius);
-            foreach(Collider collider in colliders)
+            foreach (Collider collider in colliders)
             {
                 if (collider.CompareTag("NPC"))
                 {
-                    NPCInteraction.Instance.Speak();
+                    UIManager.Instance.ToggleOptionMenu();
                 }
             }
-            
+
         }
-        
+    }
+
+    public void TogglePlayerMovement()
+    {
+        if (UIManager.Instance.OptionUIActive)
+        {
+            _playerMovement.enabled = false;
+        }
+        else
+        {
+            _playerMovement.enabled = true;
+        }
     }
 }
