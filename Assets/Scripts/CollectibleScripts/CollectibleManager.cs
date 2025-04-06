ba66e9c7c360161c22a24de18b54af55dcccbd48
Harry <harypatrascu.1@gmail.com>
2025-04-06 09:09:17 +0300

    Fixed collectible system

--- Assets/Scripts/CollectibleScripts/CollectibleManager.cs
+++ Assets/Scripts/CollectibleScripts/CollectibleManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class CollectibleManager : MonoBehaviour
 {
     public static CollectibleManager Instance { get; private set; }
-    private Dictionary<CollectibleType, int> _collectibles = new Dictionary<CollectibleType, int>();
+    private Dictionary<CollectibleType, int> _collectibleScores = new Dictionary<CollectibleType, int>();
 
     void Start()
     {
@@ -17,12 +17,24 @@ public class CollectibleManager : MonoBehaviour
         {
             Destroy(gameObject); // Destroy duplicate instance.
         }
+        // Initialize scores for all collectible types
+        foreach (CollectibleType type in System.Enum.GetValues(typeof(CollectibleType)))
+        {
+            _collectibleScores[type] = 0;
+        }
     }
 
     public void AddScore(int value, CollectibleType type)
     {
-        // Increment the value for the specified collectible type if it exists.
-        _collectibles[type] = _collectibles.ContainsKey(type) ? _collectibles[type] + value : value;
-        UIManager.Instance.UpdateText(_collectibles[type], type);
+        if (_collectibleScores.ContainsKey(type))
+        {
+            _collectibleScores[type] += value;
+            Debug.Log($"Collected {type}: {_collectibleScores[type]}");
+            UIManager.Instance.UpdateText(_collectibleScores[type], type);
+        }
+        else
+        {
+            Debug.LogWarning($"Collectible type {type} not recognized.");
+        }
     }
 }
