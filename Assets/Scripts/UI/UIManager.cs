f8d018d3e0188cefb2a74d09a8de1f9be3b3ad20
Harry <67486366+Spider-999@users.noreply.github.com>
2025-04-06 05:30:29 +0300

    Item counter

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,6 +1,8 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class UIManager : MonoBehaviour
 {
@@ -10,8 +12,21 @@ public class UIManager : MonoBehaviour
     [SerializeField] private int[] _itemCounts;
     [SerializeField] private RawImage _itemImage;
     [SerializeField] private TextMeshProUGUI _quantityText;
+    [SerializeField] private GameObject _selectionOptionIndicator;
+    [SerializeField] private GameObject _optionUI;
+    [SerializeField] private List<RectTransform> _options;
+    private int _currentOptionIndex = 0;
+    private int _itemIndex = 0;
 
-    private int itemIndex;
+    private enum Options
+    {
+        Close,
+        Attack,
+        Gift,
+        Talk
+    }
+
+    public bool OptionUIActive => _optionUI.activeSelf;
 
     private void Awake()
     {
@@ -24,8 +39,86 @@ public class UIManager : MonoBehaviour
         {
             Destroy(gameObject); // Destroy duplicate instance.
         }
+        _itemIndex = 0;
+        ChangeIndex(0); // initialising item
+    }
 
-        itemIndex = 0;
+    private void Update()
+    {
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        if(_optionUI.activeSelf)
+        {
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                MoveSelectionDown();
+            }
+            else if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                MoveSelectionUp();
+            }
+            else if (Input.GetKeyDown(KeyCode.Return))
+            {
+                ExecuteOption();
+            }
+        }
+    }
+
+    private void MoveSelectionDown()
+    {
+        _currentOptionIndex = (_currentOptionIndex + 1) % _options.Count;
+        UpdateSelectPosition();
+    }
+
+    private void MoveSelectionUp()
+    {
+        _currentOptionIndex = (_currentOptionIndex - 1 + _options.Count) % _options.Count;
+        UpdateSelectPosition();
+    }
+
+    public void UpdateSelectPosition()
+    {
+        if (_options.Count > 0)
+        {
+            Vector3 newPosition = _selectionOptionIndicator.transform.position;
+            newPosition.y = _options[_currentOptionIndex].position.y;
+            _selectionOptionIndicator.transform.position = newPosition;
+        }
+    }
+
+    private void ExecuteOption()
+    {
+        switch ((Options)_currentOptionIndex)
+        {
+            case Options.Close:
+                ToggleOptionMenu();
+                break;
+            case Options.Attack:
+                // Implement attack logic here
+                break;
+            case Options.Gift:
+                // Implement gift logic here
+                break;
+            case Options.Talk:
+                // Implement talk logic here
+                break;
+            default:
+                Debug.LogWarning($"No action defined for option: {_currentOptionIndex}");
+                break;
+        }
+    }
+
+    public void ChangeIndex(int offset) 
+    {
+        if (_itemIndex + offset < 0) { _itemIndex = _images.Length - 1; }
+        else if (_itemIndex + offset > _images.Length - 1) { _itemIndex = 0; }
+        else { _itemIndex += offset; }
+
+        _itemImage.texture = _images[_itemIndex];
+        _quantityText.text = _itemCounts[_itemIndex].ToString();
     }
 
     public void UpdateText(int value, CollectibleType type)
@@ -33,8 +126,8 @@ public class UIManager : MonoBehaviour
         switch(type)
         {
             case CollectibleType.Pizza:
-                _itemCounts[itemIndex] = value;
-                _quantityText.text = _itemCounts[itemIndex].ToString();
+                _itemCounts[0] = value;
+                if (_itemIndex == 0) { _quantityText.text = _itemCounts[_itemIndex].ToString(); }
                 break;
             default:
                 Debug.LogWarning($"No UI element for collectible type: {type}");
@@ -42,21 +135,13 @@ public class UIManager : MonoBehaviour
         }
     }
 
-    private void ChangeIndex(int offset) 
+    public void ToggleOptionMenu()
     {
-        // dealing with over/underflows
-        if (itemIndex + offset < 0) { itemIndex = _images.Length; }
-        else if (itemIndex + offset > _images.Length) { itemIndex = 0; }
-
-        // setting up item counter ui
-        itemIndex += offset;
-        SetItem(itemIndex);
-        
-    }
-
-    private void SetItem(int index) 
-    {
-        _itemImage.texture = _images[index];
-        _quantityText.text = _itemCounts[index].ToString();
+        _optionUI.SetActive(!_optionUI.activeSelf);
+        PlayerInteraction playerInteraction = FindObjectOfType<PlayerInteraction>();
+        if (playerInteraction != null)
+        {
+            playerInteraction.TogglePlayerMovement();
+        }
     }
 }
